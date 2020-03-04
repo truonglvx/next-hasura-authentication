7cb05d77995c46c70cf738414e9a7019e35df935
truong.le <truong.le@rowaviron.com>
2020-03-04 19:08:14 +0700

    add nookie

--- aviron-spa/src/components/App.js
+++ aviron-spa/src/components/App.js
@@ -6,15 +6,25 @@ import TodoPublicWrapper from "./Todo/TodoPublicWrapper";
 import OnlineUsersWrapper from "./OnlineUsers/OnlineUsersWrapper";
 
 import { useAuth0 } from "./Auth/react-auth0-spa";
+import { parseCookies } from "nookies";
 
 const App = ({ idToken }) => {
   const { loading, logout } = useAuth0();
+
+  const cookies = parseCookies();
+
   if (loading) {
     return <div>Loading...</div>;
   }
   return (
     <div>
       <Header logoutHandler={logout} />
+      <div>
+        <h3>{cookies["X-Hasura-User-Id"]}</h3>
+        <h3>{cookies["token"]}</h3>
+        <h3>{cookies["X-Hasura-User-Id"]}</h3>
+        <h3>{cookies["X-Hasura-User-Role"]}</h3>
+      </div>
       <div className="row container-fluid p-left-right-0 m-left-right-0">
         <div className="row col-md-9 p-left-right-0 m-left-right-0">
           <div className="col-md-6 sliderMenu p-30">
